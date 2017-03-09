fdff06693718b9a572bde19aa9358001ba8d904a
stas <grins38@gmail.com>
2017-03-09 23:44:39 +0000

    p24

--- p24.py
+++ p24.py
@@ -1,21 +1,22 @@
 class Number:
     def __init__(self, x):
         self.value = x
+    def __repr__(self):
+        return "%s" % self.value
         
-class Complex:
-    def __init__(self, a, b):
-        self.real = a
-        self.imag = b
-
 class Add:
     def __init__(self, l, r):
         self.left = l
         self.right = r
+    def __repr__(self):
+        return "%s + %s" % (self.left, self.right)
 
 class Multiply:
     def __init__(self, l, r):
         self.left = l
         self.right = r
+    def __repr__(self):
+        return  "%s * %s" % (self.left, self.right)
 
 
 #Add(Multiply(Number(1), Number(2)), Multiply(Number(3), Number(4)))
